ed704e1d04a530e53fdc0c262db77495bf6b1776
networkyeldar <100433043+networkyeldar@users.noreply.github.com>
2026-07-20 15:10:02 +0500

    Create ll_remove_duplicates.py

---
+++ ll_remove_duplicates.py
@@ -0,0 +1,12 @@
+def remove_duplicates(self):
+        values = set()
+        previous = None
+        current = self.head
+        while current:
+            if current.value in values:
+                previous.next = current.next
+                self.length -= 1
+            else:
+                values.add(current.value)
+                previous = current
+            current = current.next
